3e3a0639aee3f156b673d5aa3aa6f15270c89e9c
pabloignaciogutierrez28-sys <pabloignaciogutierrez28@gmail.com>
2026-06-22 12:47:08 -0300

    Update taller.py

--- MecanicaGutierrez/taller.py
+++ MecanicaGutierrez/taller.py
@@ -286,9 +286,9 @@ with tab3:
 
         else:
 
-            nuevo_id = max(
-                turno.id_turno
-                for turno in archivo_turnos
+           nuevo_id = max(
+                (turno.id_turno for turno in archivo_turnos), 
+                default=0
             ) + 1
 
             nuevo_turno = RegistroTurno(
@@ -299,6 +299,7 @@ with tab3:
                 0.0,
                 "Pendiente"
             )
+           
             archivo_turnos.append(
                 nuevo_turno
             )
